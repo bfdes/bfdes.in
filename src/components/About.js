a6aa975a15d969e3f59c49f2cddc46e5e2441277
Bruno Fernandes <bfdes@users.noreply.github.com>
2023-06-21 21:21:51 +0100

    Improve colophon

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,19 @@ export default () => (
   <Page>
     <div class="about">
       <h1>About</h1>
+      <h2>👋</h2>
       <p>
         Hello, my name is Bruno Fernandes, and I write code for fun and profit.
       </p>
       <p>
-        I studied Chemical Engineering at university. My interests include
+        I studied Chemical Engineering{" "}
+        <a href="https://www.cam.ac.uk">at university</a>. My interests include
         programming, applied maths and amateur photography -- in that order.
       </p>
       <p>
-        I work at Disney Streaming Services in London. The views I express are
-        my own and not those of my employer. And no, I can{"'"}t introduce you
-        to <a href="https://google.com/search?q=Grogu">Grogu</a>.
+        I work at Disney Streaming in London. The views I express are my own and
+        not those of my employer. And no, I can{"'"}t introduce you to{" "}
+        <a href="https://google.com/search?q=Grogu">Grogu</a>.
       </p>
       <p>
         You can find the source code for this website on{" "}
