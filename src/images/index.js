56f6476057fcd76fa73b2a3c5302669a6d8042a7
Bruno Fernandes <bfdes@users.noreply.github.com>
2025-05-14 21:59:59 +0100

    Remove comments as istanbuljs/v8-to-istanbul#198 is not reproducable

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,3 @@
-// https://github.com/istanbuljs/v8-to-istanbul/issues/198
-
 export { default as avatarWEBP } from "./avatar.webp";
 export { default as githubPNG } from "./github.png";
 export { default as rssPNG } from "./rss.png";
